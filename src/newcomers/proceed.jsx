68a25ec1f51a4fe36257432b19a505e6e647de17
WillyardUoM <willyard.jeanneton@umail.uom.ac.mu>
2023-10-21 12:42:42 +0400

    test

--- src/newcomers/proceed.jsx
+++ src/newcomers/proceed.jsx
@@ -28,13 +28,6 @@ function Proceed() {
               <span>Enter your details manually in a form</span>
             </Link>
           </div>
-          <div>
-            <Link to="/Upload_Resume" style={{ textDecoration: "none" }}>
-              <i class="fa-regular fa-file-pdf"></i>
-              <h3>Resume Scanner</h3>
-              <span>Upload Resume and retrieve information</span>
-            </Link>
-          </div>
         </div>
         <button style={{ margin: "0" }} className={prodStyles.nextBtn}>
           Next
